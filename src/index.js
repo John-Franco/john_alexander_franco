eead8fad246aa8a4fb416bddc45b67c606d34ce7
johngonzalez-gif <58112957+johngonzalez-gif@users.noreply.github.com>
2021-02-23 17:49:54 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./components/router/App";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/style.css";
-
-ReactDOM.render(<App />, document.getElementById("app"));
